72c086e12c43e45b78188e9ad8f6bf58f89771cf
RitikVishwakarma75 <ritikvishwakarma.mailbox@gmail.com>
2026-01-02 21:24:20 +0530

    change email system

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,17 +2,16 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.sendgrid.net",
     port: 587,
-    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "apikey",
+      pass: process.env.SENDGRID_API_KEY,
     },
   });
 
   await transporter.sendMail({
-    from: `"Corbett Trails Admin" <${process.env.EMAIL_USER}>`,
+    from: "no-reply@junglesafari.com",
     to,
     subject,
     html,
