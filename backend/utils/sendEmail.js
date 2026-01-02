1896e5d466f61fb293bdc8def2d99724cb74824a
RitikVishwakarma75 <ritikvishwakarma.mailbox@gmail.com>
2026-01-02 22:22:29 +0530

    sender verified for sendgrid

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   await sgMail.send({
     to,
-    from: "no-reply@junglesafari.com", // any verified sender
+    from: "vishwakarmaritik722@gmail.com", // any verified sender
     subject,
     html,
   });
