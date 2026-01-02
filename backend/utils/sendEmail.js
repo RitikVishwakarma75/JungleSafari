e7c45b80d91d4d85ab812a9a1ae902b5cd5807a2
RitikVishwakarma75 <ritikvishwakarma.mailbox@gmail.com>
2026-01-02 21:52:59 +0530

    email  debug

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,11 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
-    auth: {
-      user: "apikey",
-      pass: process.env.SENDGRID_API_KEY,
-    },
-  });
-
-  await transporter.sendMail({
-    from: "no-reply@junglesafari.com",
+  await sgMail.send({
     to,
+    from: "no-reply@junglesafari.com", // any verified sender
     subject,
     html,
   });
